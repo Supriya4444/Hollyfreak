8660849bba589cf62fc3b7fd247c0299aaeac674
Supriya4444 <168257664+Supriya4444@users.noreply.github.com>
2024-05-20 16:15:07 +0530

    none

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ movies = pd.DataFrame(movie_dict)
 
 llr = pickle.load(open('llr.pkl', 'rb'))
 
-st.image('C:\\Users\\Supriya\\OneDrive\\Pictures\\Camera Roll\\woooood.jpg',width=750)
 
 st.title(":red[Movies Recommendation System]")
 option = st.selectbox(
